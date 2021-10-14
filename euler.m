ab7931728ddf1b92ecc3e2524b159892c837df54
wg236 <wg236@cornell.edu>
2021-10-13 21:25:57 -0400

    updated comments

--- euler.m
+++ euler.m
@@ -38,17 +38,15 @@ syms x y(x) p(x) integrand(y,p)
 % optimal = vpaintegral(second_opt_int,t,0,2.41201)
 
 % Simple Harmonic Oscillator Example
+G = 0:5*10^(-3):1;
 integrand = p^2 - y^2;
 first_int = subs(integrand,p,diff(y,x));
 second_int = simplify(subs(first_int,y,@(x)x));
 initial = vpa(int(second_int,0,1))
 [new,num] = grad(integrand,x,0.3);
-first = num
 for i=1:27
     [new,num] = grad(integrand,new,0.01);
 end
-num
-G = 0:5*10^(-3):1;
 % plot(G,arrayfun(@(x) sin(x)/sin(1), G),'k');
 % hold on;
 % plot(G,arrayfun(@(x) new(x),G),'r');
@@ -63,7 +61,6 @@ hold on;
 for i=1:17
     [new,num] = grad(integrand,new,0.02);
 end
-num
 el = diff(integrand,y) - diff(diff(integrand,p),x);
 el = subs(el,p,diff(y,x));
 el = subs(el,y,new);
@@ -79,10 +76,9 @@ optimal = vpaintegral(second_opt_int,x,0,1)
 % Functional gradient descent method
 function [eq,value] = grad(integrand,sub,e)
     syms x y(x) p(x) first(p,y) second_int(x) corrector(x)
-    % The corrector is a function that is zero valued at the initial
-    % conditions and positive valued in between. It must also converge to 
-    % zero faster than the E-L equation diverges (if it diverges at all)
-    % at the initial conditions
+    % The corrector is a function that is zero valued at the initial/boundary conditions 
+    % and positive valued in between. It must also converge to zero faster than the E-L equation 
+    % diverges at the initial/boundary conditions (if it diverges at all) 
     corrector = -(x-1)*x;
     first = simplify(y-e*corrector*(diff(integrand,y) - diff(diff(integrand,p),x)));
     second = subs(first,p,diff(y,x));
